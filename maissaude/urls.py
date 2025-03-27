5466bf51468c6012726252ee91f2ed6486af0cfa
thaynararamos1 <110500874+thaynararamos1@users.noreply.github.com>
2025-03-27 19:51:34 -0300

    Página de login finalizada

--- maissaude/urls.py
+++ maissaude/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('', paginas_views.index, name='index'),
+path('login/', paginas_views.login, name='login')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
